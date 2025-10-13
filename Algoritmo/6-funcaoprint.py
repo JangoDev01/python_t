ee4847854ca157eb4b4f11acd4bb27ecac1e2955
JangoDev01 <miguelgabriel072001@gmail.com>
2025-10-13 19:38:03 +0100

    adicionando o arquivo funcao print

---
+++ Algoritmo/6-funcaoprint.py
@@ -0,0 +1,63 @@
+#### funcao print
+
+# nome = 'Miguel Gabriel'
+
+# print(nome)
+# print("Miguel Gabriel")
+
+# proficao = 'Programador'
+
+# print(proficao)
+# print(nome, '-', proficao)
+
+#######################################################
+# nome = input("Digite o seu nome: ")
+# msg = 'Olá ' + nome + '! Bem Vindo ao Namibe.'
+
+# print(msg)
+
+#######################################################
+
+# print('Imprime a mensagem e muda de linha.')
+# print('Imprime a mensagem e permanece na linha.', end='') # end='' ==> impede a quebra de linha
+# print(' Imprime a mensagem e continuo na mesma linha')
+
+#######################################################
+
+# nome = 'Maria'
+# idade = 30
+# msg_formatada = 'O nome dela é {0} e ela tem {1} anos.'.format(nome,idade)
+
+# print(msg_formatada)
+
+########################################################
+
+# nome = 'Miguel'
+# peso = 70
+# msg = f'Olá, meu nome é {nome} e eu peso {peso} kilos'
+
+# print(msg)
+
+###########################################################
+
+# a = 10
+# b = 30
+# r = f'A soma de {a} com {b} é igual a {a+b}'
+
+# print(r)
+
+##########################################################
+
+# valor = 365.575637
+# print(f'O valor é {valor:.2f}') # "variavel:.2f" ==> limite de casas depois da virgula
+
+##########################################################
+
+# valor = 365.575637
+# print(f'O valor é \'{valor:.2f}\'') # \'  \' ==> tudo depois das aspas são consideradas literais strings 
+
+#########################################################
+
+nome = 'Miguel'
+idade = 30
+print(f'Nome: {nome}\tIdade: {idade}')
