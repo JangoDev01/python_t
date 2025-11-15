238d1fea0a8fa026f3c5e764d6bc3f61b84e7b1a
JangoDev01 <miguelgabriel072001@gmail.com>
2025-11-15 23:09:09 +0100

    adicionando o arquivo tabela periodica

---
+++ prototipos/4-tabela_periodica.py
@@ -0,0 +1,242 @@
+#### Tabela periódica simples em Python
+
+## metais alcalinos
+metais_alcalinos = ("Lítio", "Sódio", "Potássio", "Rubídio", "Césio", "Frâncio")
+simbolos_metais_alcalinos = ("Li", "Na", "K", "Rb", "Cs", "Fr")
+numeros_atomicos_metais_alcalinos = (3, 11, 19, 37, 55, 87)
+
+## metais alcalino-terrosos
+metais_alcalinoterrosos = ("Berílio", "Magnésio", "Cálcio", "Estrôncio", "Bário", "Rádio")
+simbolos_metais_alcalinoterrosos = ("Be", "Mg", "Ca", "Sr", "Ba", "Ra")
+numeros_atomicos_metais_alcalinoterrosos = (4, 12, 20, 38, 56, 88)
+
+## metais de transição
+metais_de_transicao = (
+    "Escândio", "Titânio", "Vanádio", "Cromo", "Manganês", "Ferro", "Cobalto", "Níquel", "Cobre", "Zinco",
+    "Ítrio", "Zircônio", "Nióbio", "Molibdênio", "Tecnécio", "Rutênio", "Ródio", "Paládio", "Prata", "Cádmio",
+    "Háfnio", "Tântalo", "Tungstênio", "Rênio", "Ósmio", "Irídio", "Platina", "Ouro", "Mercúrio"
+)
+simbolos_metais_de_transicao = (
+    "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+    "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd",
+    "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg"
+)
+numeros_atomicos_metais_de_transicao = (
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+    72, 73, 74, 75, 76, 77, 78, 79, 80
+)
+
+## metais pós-transição
+metais_pos_transicao = (
+    "Alumínio", "Gálio", "Germânio", "Arsênio", "Índio", "Estanho", "Antimônio", "Tálio", "Chumbo", "Bismuto"
+)
+simbolos_metais_pos_transicao = ("Al", "Ga", "Ge", "As", "In", "Sn", "Sb", "Tl", "Pb", "Bi")
+numeros_atomicos_metais_pos_transicao = (13, 31, 32, 33, 49, 50, 51, 81, 82, 83)
+
+## semimetais
+semimetais = ("Boro", "Silício", "Arsênio", "Antimônio", "Telúrio")
+simbolos_semimetais = ("B", "Si", "As", "Sb", "Te")
+numeros_atomicos_semimetais = (5, 14, 33, 51, 52)
+
+## não-metais poliatômicos
+nao_metais_poliatomicos = ("Carbono", "Nitrogênio", "Oxigênio", "Fósforo", "Enxofre", "Selênio")
+simbolos_nao_metais_poliatomicos = ("C", "N", "O", "P", "S", "Se")
+numeros_atomicos_nao_metais_poliatomicos = (6, 7, 8, 15, 16, 34)
+
+## não-metais diatômicos
+nao_metais_diatomicos = ("Hidrogênio", "Hélio", "Neônio", "Argônio", "Criptônio", "Xenônio", "Radônio")
+simbolos_nao_metais_diatomicos = ("H", "He", "Ne", "Ar", "Kr", "Xe", "Rn")
+numeros_atomicos_nao_metais_diatomicos = (1, 2, 10, 18, 36, 54, 86)
+
+## gases nobres
+gases_nobres = ("Hélio", "Neônio", "Argônio", "Criptônio", "Xenônio", "Radônio", "Oganessônio")
+simbolos_gases_nobres = ("He", "Ne", "Ar", "Kr", "Xe", "Rn", "Og")
+numeros_atomicos_gases_nobres = (2, 10, 18, 36, 54, 86, 118)
+
+## lantanídeos e actinídeos
+latanideos = (
+    "Lantânio", "Cério", "Praseodímio", "Neodímio", "Promécio", "Samário", "Európio", "Gadolínio", "Térbio",
+    "Disprósio", "Hólmio", "Érbio", "Túlio", "Itérbio", "Lutécio"
+)
+simbolos_latanideos = ("La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu")
+numeros_atomicos_latanideos = (57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71)
+
+## actinídeos
+actinideos = (
+    "Actínio", "Tório", "Protactínio", "Urânio", "Netúnio", "Plutônio", "Amerício", "Cúrio", "Berquélio", "Califórnio",
+    "Einstênio", "Férmio", "Mendelévio", "Nobélio", "Laurêncio", "Rutherfórdio", "Dúbnio", "Seabórgio", "Bóhrio", "Hássio",
+    "Meitnério", "Darmstádio", "Roentgênio"
+)
+simbolos_actinideos = (
+    "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf",
+    "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs",
+    "Mt", "Ds", "Rg"
+)
+numeros_atomicos_actinideos = (89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
+    99, 100, 101, 102, 103, 104, 105, 106, 107, 108,
+    109, 110, 111
+)
+
+## elementos com propriedades desconhecidas ou sintéticos
+propriedades_desconhecidas = ("Nihônio", "Fleróvio", "Moscóvio", "Livermório", "Tenessino")
+simbolos_propriedades_desconhecidas = ("Nh", "Fl", "Mc", "Lv", "Ts")
+numeros_atomicos_propriedades_desconhecidas = (113, 114, 115, 116, 117)
+
+##
+while True:
+    print("========================================TB========================================")
+    print("Selecione uma categoria de elementos da tabela periódica: ")
+    print("[1] - Metais Alcalinos   [2] - Metais Alcalino-Terrosos    [3] - Metais de Transição")
+    print("[4] - Metais Pós-Transição   [5] - Semimetais    [6] - Não-Metais Poliatômicos")
+    print("[7] - Não-Metais Diatômicos   [8] - Gases Nobres    [9] - Lantanídeos")
+    print("[10] - Actinídeos   [11] - Propriedades Desconhecidas   [0] - Sair")
+    print("\n========================================")
+    escolha = input("Digite o número da categoria desejada: ")
+    if escolha == '1':
+        print("\n========================================")
+        print("METAIS ALCALINOS: ")
+        for elemento in metais_alcalinos: # percorre a lista de metais alcalinos
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(metais_alcalinos)): # percorre a lista de metais alcalinos e seus índices
+            if escolha2.lower() == metais_alcalinos[i].lower(): # compara a escolha do usuário com o nome do elemento
+                print(f"\nElemento: {metais_alcalinos[i]} - Símbolo: {simbolos_metais_alcalinos[i]} - Número Atômico: {numeros_atomicos_metais_alcalinos[i]}")
+                break
+        if not escolha2.lower() == metais_alcalinos[i].lower(): 
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '2':
+        print("\n========================================")
+        print("METAIS ALCALINO-TERROSOS: ")
+        for elemento in metais_alcalinoterrosos:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(metais_alcalinoterrosos)):
+            if escolha2.lower() == metais_alcalinoterrosos[i].lower():
+                print(f"\nElemento: {metais_alcalinoterrosos[i]} - Símbolo: {simbolos_metais_alcalinoterrosos[i]} - Número Atômico: {numeros_atomicos_metais_alcalinoterrosos[i]}")
+                break
+        if not escolha2.lower() == metais_alcalinoterrosos[i].lower(): 
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '3':
+        print("\n========================================")
+        print("METAIS DE TRANSIÇÃO: ")
+        for elemento in metais_de_transicao:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(metais_de_transicao)):
+            if escolha2.lower() == metais_de_transicao[i].lower():
+                print(f"\nElemento: {metais_de_transicao[i]} - Símbolo: {simbolos_metais_de_transicao[i]} - Número Atômico: {numeros_atomicos_metais_de_transicao[i]}")
+                break
+        if not escolha2.lower() == metais_de_transicao[i].lower(): 
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '4':
+        print("\n========================================")
+        print("METAIS PÓS-TRANSIÇÃO: ")
+        for elemento in metais_pos_transicao:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(metais_pos_transicao)):
+            if escolha2.lower() == metais_pos_transicao[i].lower():
+                print(f"\nElemento: {metais_pos_transicao[i]} - Símbolo: {simbolos_metais_pos_transicao[i]} - Número Atômico: {numeros_atomicos_metais_pos_transicao[i]}")
+                break
+        if not escolha2.lower() == metais_pos_transicao[i].lower(): 
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '5':
+        print("\n========================================")
+        print("SEMIMETAIS: ")
+        for elemento in semimetais:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(semimetais)):
+            if escolha2.lower() == semimetais[i].lower():
+                print(f"\nElemento: {semimetais[i]} - Símbolo: {simbolos_semimetais[i]} - Número Atômico: {numeros_atomicos_semimetais[i]}")
+                break
+        if not escolha2.lower() == semimetais[i].lower(): 
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '6':
+        print("\n========================================")
+        print("NÃO-METAIS POLIATÔMICOS: ")
+        for elemento in nao_metais_poliatomicos:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(nao_metais_poliatomicos)):
+            if escolha2.lower() == nao_metais_poliatomicos[i].lower():
+                print(f"\nElemento: {nao_metais_poliatomicos[i]} - Símbolo: {simbolos_nao_metais_poliatomicos[i]} - Número Atômico: {numeros_atomicos_nao_metais_poliatomicos[i]}")
+                break
+        if not escolha2.lower() == nao_metais_poliatomicos[i].lower(): 
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '7':
+        print("\n========================================")
+        print("NÃO-METAIS DIATÔMICOS: ")
+        for elemento in nao_metais_diatomicos:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(nao_metais_diatomicos)):
+            if escolha2.lower() == nao_metais_diatomicos[i].lower():
+                print(f"\nElemento: {nao_metais_diatomicos[i]} - Símbolo: {simbolos_nao_metais_diatomicos[i]} - Número Atômico: {numeros_atomicos_nao_metais_diatomicos[i]}")
+                break
+        if not escolha2.lower() == nao_metais_diatomicos[i].lower(): 
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '8':
+        print("\n========================================")
+        print("GASES NOBRES: ")
+        for elemento in gases_nobres:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(gases_nobres)):
+            if escolha2.lower() == gases_nobres[i].lower():
+                print(f"\nElemento: {gases_nobres[i]} - Símbolo: {simbolos_gases_nobres[i]} - Número Atômico: {numeros_atomicos_gases_nobres[i]}")
+                break
+        if not escolha2.lower() == gases_nobres[i].lower(): 
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '9':
+        print("\n========================================")
+        print("LANTANÍDEOS: ")
+        for elemento in latanideos:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(latanideos)):
+            if escolha2.lower() == latanideos[i].lower():
+                print(f"\nElemento: {latanideos[i]} - Símbolo: {simbolos_latanideos[i]} - Número Atômico: {numeros_atomicos_latanideos[i]}")
+                break
+        if not escolha2.lower() == latanideos[i].lower():
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '10':
+        print("\n========================================")
+        print("ACTINÍDEOS: ")
+        for elemento in actinideos:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(actinideos)):
+            if escolha2.lower() == actinideos[i].lower():
+                print(f"\nElemento: {actinideos[i]} - Símbolo: {simbolos_actinideos[i]} - Número Atômico: {numeros_atomicos_actinideos[i]}")
+                break
+        if not escolha2.lower() == actinideos[i].lower():
+            print("Elemento não encontrado na categoria selecionada.")
+    elif escolha == '11':
+        print("\n========================================")
+        print("PROPRIEDADES DESCONHECIDAS: ")
+        for elemento in propriedades_desconhecidas:
+            print(f"\t{elemento}")
+        print("\n========================================")
+        escolha2 = input("Escolha um dos elemenstos acima para ver mais sobre ele: ")
+        for i in range(len(propriedades_desconhecidas)):
+            if escolha2.lower() == propriedades_desconhecidas[i].lower():
+                print(f"\nElemento: {propriedades_desconhecidas[i]} - Símbolo: {simbolos_propriedades_desconhecidas[i]} - Número Atômico: {numeros_atomicos_propriedades_desconhecidas[i]}")
+                break
+        if not escolha2.lower() == propriedades_desconhecidas[i].lower():
+            print("Elemento não encontrado na categoria selecionada.")
+        print("Saindo do programa...")
+        break
+    else:
+        print("Opção inválida. Por favor, tente novamente.")
